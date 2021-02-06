3cc89895193b75c55815df3ed35fee0823e2acbf
Derek Yu <derekyu624@hotmail.com>
2021-02-06 17:10:39 -0500

    Show all tasks on frontend

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Home from '../components/home/Home';
+import Tasks from '../components/tasks/Tasks';
 
 export default (
     <Router>
         <Switch>
             <Route path='/' exact component={Home} />
+            <Route path='/tasks' exact component={Tasks} />
         </Switch>
     </Router>
 );
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'tasks/index'
+      get 'tasks', to: 'tasks#index'
       post 'tasks/create'
       get '/show/:id', to: 'tasks#show'
       delete '/destroy/:id', to: 'tasks#destroy'
