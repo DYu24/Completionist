f78221d39bf45daff9bd22ae551b42a9a8dd7ae8
Derek Yu <derekyu624@hotmail.com>
2021-02-22 13:59:40 -0500

    Add description to Lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,12 +2,21 @@ class List < ApplicationRecord
     has_many :tasks, -> { order("completed").order("title") }, dependent: :destroy
     belongs_to :user
 
+    def number_completed
+        return self.tasks.select { |t| t.completed? }.length
+    end
+
     def percent_complete
         if self.tasks.length == 0
             return 0
         end
 
-        completed_tasks = self.tasks.select { |t| t.completed? }
-        return (completed_tasks.length.to_f / self.tasks.length.to_f * 100).to_i
+        return (self.number_completed.to_f / self.tasks.length.to_f * 100).to_i
+    end
+
+    def truncated_description
+        max_length = 80
+        str = self.description
+        return str && str.length > max_length ? "#{str[0...max_length]}..." : str
     end
 end
